3fbbc97e6f23c3c1bc9e0d86594421afb56a80ee
KSksip <->
2025-10-28 15:42:12 +0100

    added vue

--- src/renderer.js
+++ src/renderer.js
@@ -27,7 +27,11 @@
  */
 
 import './index.css';
+import { createApp } from 'vue';
+import App from './App.vue';
 
 console.log(
   '👋 This message is being logged by "renderer.js", included via Vite',
 );
+
+createApp(App).mount('#app');
