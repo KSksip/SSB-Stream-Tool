e92b8ce484270b4c0bf1a87109ebc2b567aca9f3
Skip <92388016+KSksip@users.noreply.github.com>
2025-11-10 01:38:43 +0100

    added icon library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,17 +4,24 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  
+  modules: ['@nuxt/icon', '@vueuse/nuxt'],
   css: ['./app/assets/css/main.css'],
+
   vite: {    
     plugins: [      
       tailwindcss(),    
     ],  
   },
-  
+
   nitro: {
     experimental: {
       websocket: true
+    }
+  },
+
+  icon: {
+    serverBundle: {
+      collections: ['radix-icons'] // <!--- this
     }
   }
 })
