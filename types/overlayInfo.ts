47f2287baf469b98d88b38266b1a045ebbca8702
Skip <92388016+KSksip@users.noreply.github.com>
2025-11-10 01:38:33 +0100

    fixed typings to support more values and stuff

--- types/overlayInfo.ts
+++ types/overlayInfo.ts
@@ -1,15 +1,22 @@
 export type OverlayInfo = {
     setInfo: {
-        round: string,
-        bestOf: string,
+        phase: string,
+        match: string,
+        bestOf: number,
     },
-    player: [Player, ...Player[]]
+    player: [Player, Player]
 }
 
-type Player = {
+export type Player = {
+    losers: boolean,
     name: string,
     prefix: string,
     score: number,
     character: string,
     skin: string,
+    pronouns: string, 
+    country: string,
+    region: string,
 }
+
+export type PlayerPresets = { [key: string ]: Player, }
